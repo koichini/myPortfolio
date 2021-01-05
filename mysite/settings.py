76e94d8805127907649919b682aaf3281534b40c
koichini <nishikawa.ko1@gmail.com>
2021-01-05 21:36:01 +0900

    [add] form周りなど

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+# TODO https://tutorial.djangogirls.org/ja/django_templates/
 import os
 from pathlib import Path
 
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
